9b2331b36d46d19eb8e584a0217e478ec1d2add4
Wolfy Fiorini <jonwolffiorini@gmail.com>
2024-02-26 14:07:04 -0800

    initital pagesini

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "announcements", component: AnnouncementsComponent },
+  { path: "projects", component: ProjectsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProjectsComponent,
+    AnnouncementsComponent
   ],
   imports: [
     BrowserModule,
