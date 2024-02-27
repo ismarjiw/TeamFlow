57c318805470867ed9608df1695aa48434be0d86
Matthew Oshimo <moshimo90696@gmail.com>
2024-02-26 16:29:10 -0800

    small change to update project

--- backend/src/main/java/com/cooksys/groupfinal/services/impl/ProjectServiceImpl.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/impl/ProjectServiceImpl.java
@@ -67,7 +67,6 @@ public class ProjectServiceImpl implements ProjectService {
 		if(project.getDescription() != null) {
 			p.setDescription(project.getDescription());
 		}
-		p.setActive(project.isActive());
 		if(project.getTeam() != null) {
 			p.setTeam(teamMapper.requestDtoToEntity(project.getTeam()));
 		}
