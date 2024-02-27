f6637090b9751622fbbd2e28df97180b52fb6311
Wolfy Fiorini <jonwolffiorini@gmail.com>
2024-02-27 07:49:47 -0800

    added components and started login functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,11 +14,13 @@ import { AnnouncementsComponent } from './announcements/announcements.component'
     AppComponent,
     LoginComponent,
     ProjectsComponent,
-    AnnouncementsComponent
+    AnnouncementsComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl<string>(''),
+    password: new FormControl<string>('')
+  })
+
+  ngOnInit(): void {
+  }
   
+  authenticate(): void {
+    
+  }
 }
